009a9c214fa26c0022fda9bc0c8865b49b4f0eda
Guldana Zhubanysheva <ramadansmailov77@gmail.com>
2025-12-21 19:44:05 +0500

    fix: UserPoints

--- migrations/003_balance.up.sql
+++ migrations/003_balance.up.sql
@@ -1,5 +1,10 @@
+CREATE EXTENSION IF NOT EXISTS "pgcrypto";
+
 CREATE TABLE IF NOT EXISTS user_points (
-                                           user_id UUID PRIMARY KEY,
-                                           current_balance NUMERIC DEFAULT 0,
-                                           withdrawn_points NUMERIC DEFAULT 0
-);
+                                           user_id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    current_balance NUMERIC DEFAULT 0,
+    withdrawn_points NUMERIC DEFAULT 0
+    );
+
+INSERT INTO user_points (current_balance, withdrawn_points)
+VALUES (729.98, 0);
