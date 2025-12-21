ccfd1fcea63f5014aec18e2d641e639808d8009a
Guldana Zhubanysheva <ramadansmailov77@gmail.com>
2025-12-21 19:47:16 +0500

    fix: UserPoints

--- migrations/003_balance.up.sql
+++ migrations/003_balance.up.sql
@@ -6,5 +6,8 @@ CREATE TABLE IF NOT EXISTS user_points (
     withdrawn_points NUMERIC DEFAULT 0
     );
 
-INSERT INTO user_points (current_balance, withdrawn_points)
-VALUES (729.98, 0);
+INSERT INTO user_points (user_id, current_balance, withdrawn_points)
+VALUES ('11111111-1111-1111-1111-111111111111', 729.98, 0)
+    ON CONFLICT (user_id) DO UPDATE
+                                 SET current_balance = EXCLUDED.current_balance,
+                                 withdrawn_points = EXCLUDED.withdrawn_points;
