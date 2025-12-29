2ee4e92e7df2b281c4980baeaafaf29d03b7cccd
Guldana Zhubanysheva <ramadansmailov77@gmail.com>
2025-12-29 17:05:27 +0500

    fix:GetUserWithdrawals

--- models/balance.go
+++ models/balance.go
@@ -17,7 +17,7 @@ type WithdrawResponse struct {
 }
 
 type Withdrawal struct {
-	UserId      string    `json:"user_id"`
+	UserID      string    `json:"user_id"`
 	OrderNumber string    `json:"orderNumber"`
 	Sum         float64   `json:"sum"`
 	ProcessedAt time.Time `json:"processedAt"`
